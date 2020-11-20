0c4212f081efd72c8bc1d99092a4451743568eda
metrio <dlima.nyit@gmail.com>
2020-11-20 13:56:58 -0500

    Wow look at us

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
+  //creating attributes for our toy
+  const toyList = document.querySelector("#toy-collection")
+  const toyForm = document.querySelector(".add-toy-form")
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyFormContainer.style.display = "block"
     } else {
-      toyFormContainer.style.display = "none";
+      toyFormContainer.style.display = "none"
     }
   });
-});
-
-
-
-
-//creating attributes for our toy
-const toyList = document.querySelector("#toy-collection")
-const toyForm = document.querySelector(".add-toy-form")
-
 
 
 //fetching all the toys
@@ -67,33 +62,31 @@ function renderToy(toy) {
 
 //Form create
 toyForm.addEventListener("submit", function(event){
-  const toyName = form.querySelector("name")
-  const toyImgTag = form.querySelector("image")
-  const submit = form.querySelector("button")
-  
-  event.preventDefault
-  const name = event.target.name.value
-  const img = even.target.img.value
-})
+  event.preventDefault()
+
+  const toyName = toyFormContainer.querySelector("#name")
+  const toyImgTag = toyFormContainer.querySelector("#image")
+
+  const name = toyName.value
+  const img = toyImgTag.value
+
 
 //POST request for the add Toy
-function postToy(toyForm){
+
 fetch("http://localhost:3000/toys", {
   method: 'POST',
   headers: {
-    'Content-Type': '../db.json',
-    Accept: '../db.json'
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
   },
   body: JSON.stringify({
-    "name": toyForm.name.value,
-    "image": toyForm.image.value,
+    "name": name,
+    "image": img,
     "likes": 0
   })
 })
 .then(res => res.json())
 .then((toy) => {
   let newToy = renderToy(toy)
-  toyDiv.append(newToy)
+  })
 })
-}
-
