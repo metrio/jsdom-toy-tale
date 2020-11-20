494aa83f49de9a4bd7c332f583af079c229e47bb
metrio <dlima.nyit@gmail.com>
2020-11-20 11:58:34 -0500

    Slowly but surely

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //creating attributes for our toy
 const toyList = document.querySelector("#toy-collection")
+const toyForm = document.querySelector(".add-toy-form")
 
 
 
@@ -31,26 +32,68 @@ fetch("http://localhost:3000/toys")
   }) 
 })
 
-
+//Create each card for the toys
 function renderToy(toy) {
-  const toyUl = document.createElement('ul')
+  const toyDiv = document.createElement('div')
   const toyH2 = document.createElement('h2')
-  const toyA = document.createElement('a')
+  const toyP = document.createElement('p')
   const toyImgTag = document.createElement('img')
-
+  const likeBtn = document.createElement('button')
   
-  toyUl.dataset.id = toy.id
+  
+  toyDiv.className = "card"
+  toyDiv.dataset.id = toy.id
+
   toyH2.textContent = toy.name
+  toyP.textContent = `${toy.likes} Likes`
+  
+  toyImgTag.className = "toy-avatar"
   toyImgTag.src = toy.image
   toyImgTag.alt = toy.name
-  toyA.textContent = `${toy.likes} Likes`
-  
-  console.log(toy.image)
 
-  toyUl.append(toyH2, toyA)
-  toyList.append(toyUl)
+  likeBtn.className = "like-btn"
+  likeBtn.textContent = "Like <3"
+
+
+  likeBtn.addEventListener("click", function(event){
+    
+  })
+  
+
+  toyDiv.append(toyH2, toyImgTag, toyP, likeBtn)
+  toyList.append(toyDiv)
 }
 
 
+//Form create
+toyForm.addEventListener("submit", function(event){
+  const toyName = form.querySelector("name")
+  const toyImgTag = form.querySelector("image")
+  const submit = form.querySelector("button")
+  
+  event.preventDefault
+  const name = event.target.name.value
+  const img = even.target.img.value
+})
 
+//POST request for the add Toy
+function postToy(toyForm){
+fetch("http://localhost:3000/toys", {
+  method: 'POST',
+  headers: {
+    'Content-Type': '../db.json',
+    Accept: '../db.json'
+  },
+  body: JSON.stringify({
+    "name": toyForm.name.value,
+    "image": toyForm.image.value,
+    "likes": 0
+  })
+})
+.then(res => res.json())
+.then((toy) => {
+  let newToy = renderToy(toy)
+  toyDiv.append(newToy)
+})
+}
 
