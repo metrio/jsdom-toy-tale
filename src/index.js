4e117e577f3f532b2c7bbd0b836d531e788740b6
metrio <dlima.nyit@gmail.com>
2020-11-20 10:24:56 -0500

    real quick push

--- src/index.js
+++ src/index.js
@@ -13,3 +13,44 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+
+//creating attributes for our toy
+const toyList = document.querySelector("#toy-collection")
+
+
+
+//fetching all the toys
+fetch("http://localhost:3000/toys")
+.then(resp => resp.json())
+.then(toysObj => {
+  toysObj.forEach(toy => {
+    renderToy(toy)
+  }) 
+})
+
+
+function renderToy(toy) {
+  const toyUl = document.createElement('ul')
+  const toyH2 = document.createElement('h2')
+  const toyA = document.createElement('a')
+  const toyImgTag = document.createElement('img')
+
+  
+  toyUl.dataset.id = toy.id
+  toyH2.textContent = toy.name
+  toyImgTag.src = toy.image
+  toyImgTag.alt = toy.name
+  toyA.textContent = `${toy.likes} Likes`
+  
+  console.log(toy.image)
+
+  toyUl.append(toyH2, toyA)
+  toyList.append(toyUl)
+}
+
+
+
+
